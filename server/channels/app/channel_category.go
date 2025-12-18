6b8446bb9f1a5425607dcc4d10070b8d505fdeb4
akinakamu <akiyo.nakamura@stmn.co.jp>
2025-12-18 12:26:52 +0900

    feat: separate process into function

--- server/channels/app/channel_category.go
+++ server/channels/app/channel_category.go
@@ -47,31 +47,7 @@ func (a *App) GetSidebarCategoriesForTeamForUser(c request.CTX, userID, teamID s
 		}
 	}
 
-	session := c.Session()
-	// for testuser
-	if session.Roles == "" {
-		return categories, nil
-	}
-
-	hasPermissionDM := a.SessionHasPermissionTo(*session, model.PermissionCreateDirectChannel)
-	hasPermissionGM := a.SessionHasPermissionTo(*session, model.PermissionCreateGroupChannel)
-
-	if !hasPermissionDM && !hasPermissionGM {
-		filteredCategories := make(model.SidebarCategoriesWithChannels, 0)
-		filteredOrder := make(model.SidebarCategoryOrder, 0)
-
-		for _, category := range categories.Categories {
-			// If DM category contains no channels, do not return DM category
-			if category.Type == model.SidebarCategoryDirectMessages && len(category.Channels) == 0 {
-				continue
-			}
-			filteredCategories = append(filteredCategories, category)
-			filteredOrder = append(filteredOrder, category.Id)
-		}
-
-		categories.Categories = filteredCategories
-		categories.Order = filteredOrder
-	}
+	a.filterSidebarCategories(c, categories)
 
 	return categories, nil
 }
---
+++ server/channels/app/channel_category_filter.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+package app
+
+import (
+	"github.com/mattermost/mattermost/server/public/model"
+	"github.com/mattermost/mattermost/server/public/shared/request"
+)
+
+func (a *App) filterSidebarCategories(c request.CTX, categories *model.OrderedSidebarCategories) {
+	session := c.Session()
+
+	// for testuser
+	if session.Roles == "" {
+		return
+	}
+
+	canCreateDirectChannel := a.SessionHasPermissionTo(*session, model.PermissionCreateDirectChannel)
+	canCreateGroupChannel := a.SessionHasPermissionTo(*session, model.PermissionCreateGroupChannel)
+
+	if canCreateDirectChannel || canCreateGroupChannel {
+		return
+	}
+
+	filteredCategories := make(model.SidebarCategoriesWithChannels, 0)
+	filteredOrder := make(model.SidebarCategoryOrder, 0)
+
+	for _, category := range categories.Categories {
+		// When the user lacks permissions to create DMGM and DM category is empty, remove DM category
+		if category.Type == model.SidebarCategoryDirectMessages && len(category.Channels) == 0 {
+			continue
+		}
+		filteredCategories = append(filteredCategories, category)
+		filteredOrder = append(filteredOrder, category.Id)
+	}
+
+	categories.Categories = filteredCategories
+	categories.Order = filteredOrder
+}
