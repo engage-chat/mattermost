91b4811ff8e695311860c78111516f6cd8ffeca2
naka <235416384+akinakamu@users.noreply.github.com>
2026-07-03 11:06:04 +0900

    test: add new props for test

--- webapp/channels/src/components/sidebar/sidebar.test.tsx
+++ webapp/channels/src/components/sidebar/sidebar.test.tsx
@@ -79,6 +79,7 @@ describe('components/sidebar', () => {
         isKeyBoardShortcutModalOpen: false,
         userGroupsEnabled: false,
         canCreateCustomGroups: true,
+        readyToRender: true,
         actions: {
             createCategory: jest.fn(),
             fetchMyCategories: jest.fn(),
